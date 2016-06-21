cf16281b2a62e07baf348d44cf5c7685ee9fd8f3
Minyoung Ryoo <minyoung.ryoo@gmail.com>
2016-06-21 14:23:32 -0400

    class exercises for dynamic website

---
+++ Week2/Day2/sinatra/app.rb
@@ -0,0 +1,51 @@
+require "sinatra"
+
+get "/" do
+	"Signature"
+end
+
+# get "/hi" do
+# 	"Hello, world!"
+# end
+
+get "/about" do
+	erb(:about)
+end
+
+
+get "/hi" do
+	@name = "Min"
+	erb(:hipage)
+	end
+
+get "/recipe" do
+	@name = "Red Velvet Cake"
+	@ingredients = [
+		"eggs",
+		"flour",
+		"red food coloring",
+		"cocoa",
+		"buttermilk",
+		"sugar",
+		"cream cheese"
+	]
+	erb(:recipe_page)
+end
+
+users = {
+	"khalifenizar" => {:name => "Nizar Khalife", :email => "naaazz@ironhack.com"},
+	"ag" => {:name => "Alida Gagliuffi", :email => "agagag@ironhack.com"},
+	"alia" => {:name => "Ali Poonawala", :email => "aaaaa@ironhack.com"}
+}
+
+get "/users/:username" do
+	@name = params[:username]
+
+	if @name == "min"
+		erb(:whiskey)
+	else
+		@info = users[@name]
+		erb(:user_profile)
+	end
+end
+
