378ee336041881159ec32030cae3e94d796bc7ea
Minyoung Ryoo <minyoung.ryoo@gmail.com>
2016-07-06 10:32:18 -0400

    adding id to url

--- Week4/timetracking/app/controllers/projects_controller.rb
+++ Week4/timetracking/app/controllers/projects_controller.rb
@@ -3,5 +3,18 @@ class ProjectsController < ApplicationController
 		@projects_arr = Project.order(created_at: "desc")
 								.limit(10)
 		render 'index'
+
+		# @projects = Project.last_created_projects
+	end
+
+	def show
+		@my_project = Project.find(params[:id])
+
+		if @my_project == nil
+			render "no_projects_found"
+		end
+		
+		render 'show'
 	end
 end
+
