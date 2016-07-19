c30d1a83fb677b3f4dfde18255bb177a52375ec0
Minyoung Ryoo <minyoung.ryoo@gmail.com>
2016-07-19 09:58:57 -0400

    Updates to all exercises until week 6

--- Week5/Day2/spotify/spotify.js
+++ Week5/Day2/spotify/spotify.js
@@ -21,8 +21,6 @@ $(document).ready (function() {
 			
 			$('.btn').click(function(event){
 				var artistId = $(event.currentTarget).data("id");
-				var divId = $('.container').data("id");
-				// console.log("https://api.spotify.com/v1/artists/" + artistId + "/albums")
 				$.ajax({
 					type: "GET",
 					url: "https://api.spotify.com/v1/artists/" + artistId + "/albums",
@@ -54,9 +52,7 @@ $(document).ready (function() {
 
 
 			$('.btn').click(function(event){
-			// $('#myModal').modal("show");
 				var albumId = $(event.currentTarget).data("id");
-				// var divId = $('.container').data("id");
 				console.log(albumId);
 				$.ajax({
 					type: "GET",
@@ -76,19 +72,7 @@ $(document).ready (function() {
 		response.items.forEach(function(track){
 			$(`.modal-body`).append(`<h2>Title: ${track.name}</h2>`);
 			$('#myModal').modal('show');
-			// console.log(track.preview_url);
 			$(`.modal-body`).append(`<audio src="${track.preview_url}" controls></audio>`);
-			
-
-			// if (track.images.length >0){
-			// $(`.modal-body`).append(`<img src=${track.images[1].url}>`);
-			// }else{
-			// 	$(`.modal-body`).append(`<p>No Images Available</p>`);
-			// }
-
-			// $(`.modal-body`).append(`<div><button type="button" data-id=${track.id} class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-			//   ${track.name}
-			// </button></div>`);
 
 		})
 	}
@@ -109,28 +93,7 @@ $(document).ready (function() {
 		console.log(error.responseText);
 	}
 
-	// function showAlbums
-
 	$(".js-search-form").on("submit", fetchData);
-// 	// $(".classname").on("click", showAlbums);
-// $(".classname").onclick = alert("It worked!");
-
-
-	// function showList(event){
-	// 	event.preventDefault();
-	// 	var artist_name = $('#search').val();
-	// 	$.ajax({
-	// 	  type: "Post",
-	// 	  // data: newArtist,
-	// 	  url: "https://api.spotify.com/v1/search?type=artist&query=" + artist_name,
-	// 	  success: showExistingList
-	// 	  error: handleError
-	// 	})
-	// }
-
-	// function showExistingList(response){
-
-	// }
 
 });
 
